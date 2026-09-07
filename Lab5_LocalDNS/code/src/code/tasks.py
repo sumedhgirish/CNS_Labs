0948f884bb52f258fad68c0b72b0f6640de64e81
Sumedh Girish <sumedh.girish@gmail.com>
2026-09-07 11:28:35 +0530

    [WEEK5] Starting work on task 1
    Trying to sniff DNS records

---
+++ Lab5_LocalDNS/code/src/code/tasks.py
@@ -0,0 +1,22 @@
+from scapy.all import *
+from scapy.layers.dns import DNS
+
+ATTACKER_NS = "ns.attacker32.com"
+ATTACKER_IFACE = "br-fa8ae839be42"
+
+
+class Task:
+    @staticmethod
+    def _1():
+
+        TARGET = "www.example.com"
+
+        def spoof_dns(pkt):
+            if DNS in pkt and TARGET in pkt[DNS].qd.qname.decode("utf-8"):
+                print(pkt.summary())
+
+        _ = sniff(iface=ATTACKER_IFACE, prn=spoof_dns, filter="dns")
+
+
+if __name__ == "__main__":
+    Task()._1()
